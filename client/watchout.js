6e8ec46a85c4f9509896929dbd1df46e37d50233
alberth8 <hanalbert87@gmail.com>
2016-04-30 11:20:27 -0700

    completed everything but css3 animation

--- client/watchout.js
+++ client/watchout.js
@@ -6,14 +6,15 @@ $(window).load(function() {
 var settings = {
   width: 750,
   height: 500,
-  n: 50,
+  numEnemies: 50,
   score: 0,
   highScore: 0,
   collisions: 0,
-  isHit: 0,
+  isHit: false,
   delay: 1000,
-  svg: d3.select('body').append('svg') // svg refers to container
-}
+  svg: d3.select('body').append('svg'), // svg refers to container
+  size: 25
+};
 
 var game = {
   init: function() {
@@ -24,51 +25,56 @@ var game = {
   createCircles: function() {
     var dataset = [];
     settings.svg.attr('width', settings.width).attr('height', settings.height);
-    for (var i = 0; i < settings.n; i++) {
-     dataset.push(25);//(Math.random() * 1) + 5);
+    for (var i = 0; i < settings.numEnemies; i++) {
+      dataset.push(settings.size);//(Math.random() * 1) + 5);
     }
+    // creating enemies
     var circles = settings.svg.selectAll('circle')
       .data(dataset)
-      .enter()
-      .append('circle');
-    circles.attr('cx', function(d, i) {
+      .enter() // select placehol ders that have a value but no key (yet)
+      .append('circle'); // sets key 
+    circles.attr('cx', function(d, i) { // setting attributes for enemies
       return 25;//(i * 50) + 25;
-      })
+    })
       .attr('cy', settings.height / 2)
       .attr('r', function(d) {
         return d;
       }).classed({'asteroid': true})
-      .style('image', 'url(asteroid.png)');
-      //.attr('image', 'url(asteroid.png)');
-    //d3.selectAll('circle').attr('image', 'url(asteroid.png)');
+      .style('image', 'url(asteroid.png)'); // failed attempt
   },
 
-  cycles: function() {
+  // update / tracking functions
+  cycles: function() { 
+    // moving enemies
     setInterval(function() { 
-      // var randx = Math.random() * settings.width;
-      // var randy = Math.random() * settings.width;
+      // movement
       d3.selectAll('circle').each(function() { // moving each circles to random positions
         d3.select(this)
-        .transition()
-        .tween('circle', function(d, i, a) {
+        .transition() 
+        .tween('circle', function() { // tween calls during every point of transition (pixel), *not* every second
           return function() {
-
-            if((Math.abs(this.getAttribute('cx') -  player.getX()) < 25) && (Math.abs(this.getAttribute('cy') - player.getY()) < 10 )) {
-              game.hitCheck();
+            // checks if in 25x25 square buffer of center
+            if ((Math.abs(this.getAttribute('cx') - player.getX()) < 25) && (Math.abs(this.getAttribute('cy') - player.getY()) < 10 )) {
+              // changes collision state to true
+              game.hitCheck(); 
               console.log('test!');
             }
-          }
+          };
         })
-        .attr('cx', Math.random() * settings.width)
-        .attr('cy', Math.random() * settings.height)
-        .duration(1500);
+        .attr('cx', (Math.random() * (settings.width + 200)) - 100)
+        .attr('cy', (Math.random() * (settings.height + 200)) - 100)
+        .duration(1500); // time lapse from point A to point B
       });
-    }, 1000);
-    setInterval(function() {
+    }, 1000); // idle time (between movements)
+
+    // scoring - updating score box
+    // originally, collision checker repeated too quickly in tweens, and no way to slow down,
+    // so moved to separate setInterval().
+    setInterval(function() { // 
       game.scoring();
-      d3.select('.highscore').text("Highscore: " + settings.highScore);
-      d3.select('.score').text("Score: " + settings.score);
-      d3.select('.collisions').text("Collisions: " + settings.collisions);
+      d3.select('.highscore').text('Highscore: ' + settings.highScore);
+      d3.select('.score').text('Score: ' + settings.score);
+      d3.select('.collisions').text('Collisions: ' + settings.collisions);
       game.collision();
     }, 100);
   },
@@ -79,10 +85,10 @@ var game = {
     settings.isHit = true;
   },
   collision: function() {
-    if(settings.isHit) {
+    if (settings.isHit) {
       console.log('test2');
       settings.collisions++;
-      if(settings.score > settings.highScore) {
+      if (settings.score > settings.highScore) {
         settings.highScore = settings.score;
       }
       settings.score = 0;
@@ -93,11 +99,12 @@ var game = {
   scoring: function() {
     settings.score++;
   }
-}
+};
 
 var player = {
-  plX: settings.width / 2,
-  plY: settings.height / 2,
+  // x, y coordinate of player
+  posX: settings.width / 2,
+  posY: settings.height / 2,
   init: function() {
     player.createPlayer();
   },
@@ -120,17 +127,19 @@ var player = {
       .attr('fill', 'red')
       .call(drag);
   },
+  // setter functions - used to set `posX`
   setX: function(value) {
-    player.plX = value;
+    player.posX = value;
   },
   setY: function(value) {
-    player.plY = value;
+    player.posY = value;
   },
+  // getter functions - retrieve value
   getX: function() {
-    return player.plX //= player.ellipse.attr('cx') || 0;
+    return player.posX; //= player.ellipse.attr('cx') || 0;
   },
   getY: function() {
-    return player.plY //= player.ellipse.attr('cy') || 0;
+    return player.posY; //= player.ellipse.attr('cy') || 0;
   }
 
-}
+};
