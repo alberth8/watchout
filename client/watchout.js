a0037290b278c8793a526e30a19ff7e178d41553
alberth8 <hanalbert87@gmail.com>
2016-04-29 21:29:09 -0700

    Working on collision

--- client/watchout.js
+++ client/watchout.js
@@ -1,10 +1,11 @@
-var n = 10;
+var n = 1;
+var collisions = 0;
 var dataset = [];
 for (var i = 0; i < n; i++) {
   dataset.push((Math.random() * 25) + 5);
 }
-var width = 500;
-var height = 500;
+var width = 750; // cx
+var height = 500; // cy
 
 var svg = d3.select('body').append('svg'); // svg refers to container
 svg.attr('width', width).attr('height', height);
@@ -20,48 +21,67 @@ circles.attr('cx', function(d, i) {
   .attr('cy', height / 2)
   .attr('r', function(d) {
     return d;
-  });
+  }).attr('id', 'asteroid');
 
-setInterval(function() { 
-  circles.each(function() {
-    d3.select(this).transition().attr('cx', Math.random() * 500)
-    .attr('cy', Math.random() * 500)
-    .duration(500);
-  }); 
-}, 500);
-
-var movement = function() {
-
-};
-
+var drag = d3.behavior.drag()
+          .on('drag', function () {
+            d3.select(this)
+            .attr('cx', (d3.event.x > width) ? width : ( (d3.event.x < 0) ? 0 : (d3.event.x) )) //d3.event.x is the x-value of the mouse
+            .attr('cy', (d3.event.y > height) ? height : ( (d3.event.y < 0) ? 0 : (d3.event.y) )); 
+          });
 
 var ellipse = svg.append('ellipse')
-                .attr('cx', 250)
-                .attr('cy', 250)
+                .attr('cx', width / 2)
+                .attr('cy', height / 2)
                 .attr('rx', 25)
                 .attr('ry', 10)
-                .attr('fill', 'red');
+                .attr('fill', 'red')
+                .call(drag);
 
-// creates a drag FUNCTION
-var drag = d3.behavior.drag()
-          .origin(function(d) {
-            console.log(d);
-            return d;
-          })
-          .on('drag', dragMove);
+setInterval(function() { 
+  circles.each(function() { // moving each circles to random positions
+    d3.select(this)
+    .transition()
+    .tween('circle', function (data, index) {
+      console.log(data);
+      // var curX = Math.floor(this.attr('cx'));
+      // var curY = Math.floor(this.attr('cy'));
 
-var dragMove = function (d) {
-  d3.select(this)
-    .attr("cx", d.x)
-    .attr("cy", d.y);
+      // // only update if circle has moved at least a pixel in x and y
+      // // directions
+      // if (Math.abs(this.attr('cx') - ellipse.attr('cx')) < 25 && Math.abs(this.attr('cy') - ellipse.attr('cy')) < 25) {
+      //   collisions ++;
+      // }
+    })
+    .attr('cx', Math.random() * width)
+    .attr('cy', Math.random() * height)
+    .duration(500);
+  });
+}, 500);
+
+var tween = function () {
+  // var lastX = 0;
+  // var lastY = 0;
+   
+  // This function returned by tracker is what will execute at each 'tick'
+  // in the transition animation
+  return function() {
+
+  };
 };
 
-// console.log(drag);
 
-// drag.on('drag', function() {
-//   ellipse.attr('rx', this.loc)
-// })
-
-
-// selection.on('dragstart', function () 
-
+// // selecting all 'circle' svg elements and tracking x-y coordinates
+// setInterval(function() {
+//   d3.selectAll('circle').each(function(d, i) {
+//     var thing = d3.select(this);
+//     if (thing.attr('id') === 'asteroid') {
+//     // console.log('asteroid y-coord', thing.attr('cy') );
+//     // console.log('ellipse y-coord', ellipse.attr('cy'));
+//       if (Math.abs(thing.attr('cx') - ellipse.attr('cx')) < 25 && Math.abs(thing.attr('cy') - ellipse.attr('cy')) < 25) {
+//         collisions++;
+//         console.log(collisions);
+//       }
+//     }
+//   });
+// }, 100);
